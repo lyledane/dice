3faa8e442324d7453c64dd89a376999bd610a8eb
lyledane <76196715+lyledane@users.noreply.github.com>
2021-04-26 09:20:39 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Dice Activity'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
         body: DicePage(),
       ),
