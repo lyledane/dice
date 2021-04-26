1dc48b1a1a65270f3958036cf22d2fb219e495bc
lyledane <76196715+lyledane@users.noreply.github.com>
2021-04-26 10:22:33 +0800

    changing of text

--- lib/main.dart
+++ lib/main.dart
@@ -5,35 +5,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigoAccent,
+        backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dice Activity'),
-          backgroundColor: Colors.blue,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
-}
-
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftSidedice = Random().nextInt(6) + 1;
-  int rightSidedice = Random().nextInt(6) + 1;
   void testFunction() {
-    leftSidedice = Random().nextInt(6) + 1;
-    rightSidedice = Random().nextInt(6) + 1;
-  }
-
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
